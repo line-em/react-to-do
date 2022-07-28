57b5c70d101a1727a94aabf3fb494c8a7735e162
Line-em <alineemily@hotmail.com>
2022-07-28 18:43:58 +0100

    Finish adding the elements as-is from my previous project.

--- src/ThemeSwitch.jsx
+++ src/ThemeSwitch.jsx
@@ -1,28 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MoonStars, Sun } from "phosphor-react";
 
-export default function ThemeSwitch(props) {
+export default function ThemeSwitch() {
+	//Tooltip
 	const [active, setActive] = React.useState(false);
-
 	const tooltipStyle = {
 		display: active ? "block" : "none"
 	};
+
+	// Theming
+	const [theme, setTheme] = useState(
+		localStorage.getItem("theme") ||
+			(window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+	);
+
+	const toggleTheme = () => {
+		const newTheme = theme === "light" ? "dark" : "light";
+		setTheme(newTheme);
+		localStorage.setItem("theme", newTheme);
+		document.querySelector("html").setAttribute("data-theme", newTheme);
+	};
+
+	useEffect(() => {
+		document.querySelector("html").setAttribute("data-theme", theme);
+	}, []);
 
 	return (
 		<nav>
 			<button
 				className="iconNight"
-				onClick={props.toggleTheme}
+				onClick={toggleTheme}
 				onMouseEnter={() => setActive(true)}
 				onMouseLeave={() => setActive(false)}
 			>
-				{props.theme === "light" ? (
+				{theme === "light" ? (
 					<Sun weight="fill" size={24} />
 				) : (
 					<MoonStars weight="fill" size={24} />
 				)}
 				<figcaption style={tooltipStyle} className="tooltip tooltip_style">
-					{props.theme} Theme
+					{theme} Theme
 				</figcaption>
 			</button>
 		</nav>
