3a7c54b9f0ef0b639fb3c6dc4d5229242fa31e69
Line-em <alineemily@hotmail.com>
2022-08-03 15:46:02 +0100

    again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://line-em.github.io/react-to-do/"
+	base: "/react-to-do/"
 });
