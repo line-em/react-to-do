2776283d2909aadef00d4e0c0cbb5c35e8f4daa3
Line-em <alineemily@hotmail.com>
2022-08-03 15:12:07 +0100

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 		<div className="wrapper">
 			<App />{" "}
 			<footer>
-				Coded with 💚 by{" "}
+				Coded with 🤍 and tea by{" "}
 				<a
 					href="https://github.com/line-em"
 					target="_blank"
